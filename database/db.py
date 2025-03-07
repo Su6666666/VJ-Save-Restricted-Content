4619de25dd447c3e274600809ac9894bd2bb99c5
textleech001 <textleech@gmail.com>
2025-03-07 12:09:15 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -42,24 +42,3 @@ class Database:
 
 db = Database(DB_URI, DB_NAME)
 
-import sqlite3
-
-conn = sqlite3.connect("database.db")
-cursor = conn.cursor()
-
-def is_subscribed(user_id):
-    cursor.execute("SELECT * FROM subscribers WHERE user_id = ?", (user_id,))
-    return cursor.fetchone() is not None
-
-def subscribe(user_id):
-    cursor.execute("INSERT INTO subscribers (user_id) VALUES (?)", (user_id,))
-    conn.commit()
-
-def unsubscribe(user_id):
-    cursor.execute("DELETE FROM subscribers WHERE user_id = ?", (user_id,))
-    conn.commit()
-
-def is_premium(user_id):
-    cursor.execute("SELECT * FROM premium_members WHERE user_id = ?", (user_id,))
-    return cursor.fetchone() is not None
-
