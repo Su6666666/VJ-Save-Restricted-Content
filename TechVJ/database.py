d9a439367499d08d8dbe81b1af607edf298f9f73
textleech001 <textleech@gmail.com>
2025-03-07 12:10:32 +0530

    Create database.py

---
+++ TechVJ/database.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+conn = sqlite3.connect("database.db")
+cursor = conn.cursor()
+
+def is_subscribed(user_id):
+    cursor.execute("SELECT * FROM subscribers WHERE user_id = ?", (user_id,))
+    return cursor.fetchone() is not None
+
+def subscribe(user_id):
+    cursor.execute("INSERT INTO subscribers (user_id) VALUES (?)", (user_id,))
+    conn.commit()
+
+def unsubscribe(user_id):
+    cursor.execute("DELETE FROM subscribers WHERE user_id = ?", (user_id,))
+    conn.commit()
+
+def is_premium(user_id):
+    cursor.execute("SELECT * FROM premium_members WHERE user_id = ?", (user_id,))
+    return cursor.fetchone() is not None
+
